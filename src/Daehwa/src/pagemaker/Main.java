01495d32fa5f8b4c88523c234aac7dc25cc09d84
김대화 <cd5465@hanmail.net>
2025-05-07 21:54:03 +0900

    Add files via upload

---
+++ src/Daehwa/src/pagemaker/Main.java
@@ -0,0 +1,9 @@
+package pagemaker;
+
+import pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hyuki@example.com", "Welcome.html");
+    }
+}
