de25a4c0481c0ef903c53e92c5b8d86c1ef7ecae
김태현 <xogusrla7@gmail.com>
2025-05-09 11:45:01 +0900

    수정

--- src/taehyun/Strategy_Pattern_Not_Used/NStrategy_Pattern_Test.java
+++ src/taehyun/Strategy_Pattern_Not_Used/NStrategy_Pattern_Test.java
@@ -1,14 +1,14 @@
 public class NStrategy_Pattern_Test {
     public static void main(String[] args) {
-        RouteStrategy shortest = new ShortestPathStrategy();
+        NRouteStrategy shortest = new NShortestPathStrategy();
         shortest.buildRoute("서울", "부산");
         System.out.println("예상 소요 시간: " + shortest.estimateRoute("서울", "부산") + "분\n");
 
-        RouteStrategy trafficAware = new TrafficAwareStrategy();
+        NRouteStrategy trafficAware = new NTrafficAwareStrategy();
         trafficAware.buildRoute("서울", "부산");
         System.out.println("예상 소요 시간: " + trafficAware.estimateRoute("서울", "부산") + "분\n");
 
-        RouteStrategy scenic = new ScenicRouteStrategy();
+        NRouteStrategy scenic = new NScenicRouteStrategy();
         scenic.buildRoute("서울", "부산");
         System.out.println("예상 소요 시간: " + scenic.estimateRoute("서울", "부산") + "분");
     }
