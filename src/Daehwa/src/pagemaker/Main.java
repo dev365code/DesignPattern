6708518ca00528aa34b5a01a5a1cbac234593f0a
Daehwa Kim <cd5465@hanmail.net>
2025-06-05 08:25:08 +0900

    Delete src/Daehwa/src/pagemaker directory

--- src/Daehwa/src/pagemaker/Main.java
+++
@@ -1,9 +0,0 @@
-package pagemaker;
-
-import pagemaker.PageMaker;
-
-public class Main {
-    public static void main(String[] args) {
-        PageMaker.makeWelcomePage("hyuki@example.com", "Welcome.html");
-    }
-}
